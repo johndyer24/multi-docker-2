68c87dc4f364c21324abd57e63744bd14c3349c2
johndyer24 <johndyer24@gmail.com>
2020-03-27 18:41:27 -0400

    Test changes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Other page!!!
+      Other page!
       <Link to="/">Go back home</Link>
     </div>
   );
